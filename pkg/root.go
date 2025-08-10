2da75c25b953bfca7fcb77d031bbbd3de85592a2
sj0n <hariz@proton.me>
2025-08-10 16:52:18 +0800

    chore: bump version to 1.4.0

--- pkg/root.go
+++ pkg/root.go
@@ -10,6 +10,6 @@ var (
 	Format   string
 	RootCmd  = &cobra.Command{
 		Long:    "Transcribe audio files using Deepgram, OpenAI and AssemblyAI models.",
-		Version: "1.3.0",
+		Version: "1.4.0",
 	}
 )
